4b943a5c3313895c6849ccae1a8966d8c88a006a
tobubble <1459179001@qq.com>
2020-09-15 18:15:57 +0800

    加入了home页面

---
+++ src/views/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="home">
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    methods: {
+      logout() {
+        window.sessionStorage.clear()
+        this.$router.push("/login")
+        this.$message.success('退出成功')
+      }
+    }
+  }
+</script>
+
+<style lang='less' scoped>
+
+</style>
